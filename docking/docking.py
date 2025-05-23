4274088b0e3d99d32e189e4f95bdab9732963713
ylyu28 <yalilyu28@gmail.com>
2025-05-23 10:41:27 +0100

    Fixed protein and ligand structure paths

--- docking/docking.py
+++ docking/docking.py
@@ -16,8 +16,8 @@ def prepare_ligand(smile_string):
     writer functions in meeko to save the prepared ligand molecules into PDBQT files
     """
 
-    pdbqt_path = "/Users/yalilyu/Desktop/code/DrugSynthMC2/docking/ligand.pdbqt"
-    ligand_sdf_path = "/Users/yalilyu/Desktop/code/DrugSynthMC2/docking/ligand.sdf"
+    pdbqt_path = "./docking/ligand.pdbqt"
+    ligand_sdf_path = "./docking/ligand.sdf"
     
     with open(pdbqt_path, 'w') as file:
     # erase log contents
@@ -105,7 +105,7 @@ def vina_multiprocessing(protein, ligand_pdbqt_path, config_file, num_files):
     """
     
     start_time = time.time()
-    log_path = f'/Users/yalilyu/Desktop/code/DrugSynthMC2/docking/{protein}.txt'
+    log_path = f'./docking/{protein}.txt'
 
     with open(log_path, 'w') as f:
         pass  # create an empty file
@@ -116,8 +116,7 @@ def vina_multiprocessing(protein, ligand_pdbqt_path, config_file, num_files):
         ligand_pdbqt_path,
         config_file,
         os.path.join(
-            "/Users/yalilyu/Desktop/code/DrugSynthMC2/docking/ar/ar_structures",
-            f"{protein}_clusters",
+            "./docking/ar/ar_structures",
             f"{protein}_clusters_{i}.pdbqt"
         ),
         i
@@ -165,91 +164,9 @@ def vina_multiprocessing(protein, ligand_pdbqt_path, config_file, num_files):
                         affinities.append(affinity_value)
                     except ValueError:
                         pass
-
+    print(f"min affinity is {min(affinities)}")
     return min(affinities) if affinities else None
 
-
-# def vina_docking(protein, ligand_pdbqt_path, config_file, num_files):
-#     """
-#     Convert ligand smiles string to pdbqt, then batch Docking the ligand to cluster structures of a specified protein, write affinties table to log, then extract the most favourable affinity value.
-#     """
-#     start_time = time.time()
-    
-#     with open(f'/Users/yalilyu/Desktop/python_test_docking/{protein}.txt', 'w') as file:
-#     # erase log contents
-#         pass
-
-#     for i in range(1, num_files + 1):
-#         protein_file = os.path.join(
-#             "/Users/yalilyu/Desktop/python_test_docking/",
-#             f"{protein}_clusters",
-#             f"{protein}_clusters_{i}.pdbqt"
-#         )
-
-#         # Run Vina and capture output
-#         command = [
-#         "vina",
-#         "--receptor", protein_file,
-#         "--ligand", ligand_pdbqt_path,
-#         "--config", config_file
-#         ]
-
-#         result = subprocess.run(command, capture_output=True, text=True, check=True)
-#         stdout = result.stdout
-
-#         # Extract only the affinity table
-#         affinities_table = extract_affinity_table_from_stdout(stdout)
-    
-#         log_path = f'/Users/yalilyu/Desktop/python_test_docking/{protein}.txt'
-    
-#         if not os.path.exists(log_path):
-#             with open(log_path, 'w') as f:
-#                 pass  # create an empty file
-#             print(f"Created empty file: {log_path}")
-
-#     # Append only the table into log
-#         with open(log_path, 'a') as log:
-#             log.write(f"\n--- Affinity Table for {protein} Cluster {i} ---\n")
-#             log.write(affinities_table)
-#             log.write('\n\n')
-
-#         print(f"{protein} cluster {i} results appended to log.")
-    
-#     end_time = time.time()
-#     print("Docking time used", end_time - start_time)
-
-#     affinities = []
-#     in_table = False
-
-#     with open(log_path, 'r') as f:
-#         for line in f:
-#             line_strip = line.strip()
-
-#             # Detect start of a table
-#             if line_strip.startswith('--- Affinity Table'):
-#                 in_table = True
-#                 continue
-
-#             # Detect separator line that precedes data
-#             if in_table and line_strip.startswith('-----+'):
-#                 continue
-
-#             # Exit table when encountering a new table
-#             if in_table and line_strip.startswith('--- Affinity Table'):
-#                 in_table = False
-#                 continue
-
-#             # Collect affinity values
-#             if in_table:
-#                 parts = line_strip.split()
-#                 if len(parts) >= 2:
-#                     try:
-#                         affinity_value = float(parts[1])
-#                         affinities.append(affinity_value)
-#                     except ValueError:
-#                         pass
-
-#     return min(affinities) if affinities else None
 
 def docking_score(protein, smile_string, config_file, num_files):
     """
@@ -257,17 +174,7 @@ def docking_score(protein, smile_string, config_file, num_files):
     """
     ligand_pdbqt_path, ligand_pdbqt_content = prepare_ligand(smile_string)
     best_affinity = vina_multiprocessing(protein, ligand_pdbqt_path, config_file, num_files)
-    affinity_score = 1 - 1 / (1 + math.exp(-(best_affinity +5)))
-    
+    affinity_score = 1 - 1 / (1 + math.exp(-(best_affinity +6)))
+    # print(f"best affinity:{best_affinity}, affinity_score:{affinity_score}")
     return best_affinity, affinity_score
-
-# def docking_score(protein, smile_string, config_file, num_files):
-#     """
-#     Docking the generated ligand onto the protein and return the lipinski score as a component of the reward, and erase log
-#     """
-#     ligand_pdbqt_path, ligand_pdbqt_content = prepare_ligand(smile_string)
-#     best_affinity = vina_docking(protein, ligand_pdbqt_path, config_file, num_files)
-#     affinity_score = 1 - 1 / (1 + math.exp(-(best_affinity +5)))
-    
-#     return best_affinity, affinity_score
 
