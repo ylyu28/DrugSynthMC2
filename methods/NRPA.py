7f93412c028fb80cbd7a6430abbb198735446331
ylyu28 <yalilyu28@gmail.com>
2025-12-15 10:06:04 +0000

    upload NRPA.py

---
+++ methods/NRPA.py
@@ -0,0 +1,126 @@
+from models.SMILESgen_cyanide import State
+from tools.resultSaver import writeline
+import time
+import random
+import math
+import numpy as np
+import copy
+
+
+
+class NRPA:
+    def __init__(self):
+        self.timeout = -1.0
+        self.registerName= ""
+        self.start_time = time.perf_counter()
+
+
+    def playout(self, st:State, policy):
+        while not st.terminal():
+            moves = st.legal_moves()
+            weight = 0
+            for mv in moves:
+                weight = weight + math.exp(policy[st.code(mv)]+st.heuristic(mv))
+
+            stop = random.random() * weight
+            move = 0
+            weight = 0
+            while True:
+                weight = weight + math.exp(policy[st.code(moves[move])]+st.heuristic(moves[move]))
+                if weight >= stop:
+                    break
+                move = move + 1
+            st.play(moves[move])
+
+        return st
+
+
+    def adapt(self, st:State, policy):
+        """
+        Policy is adapted on the best current seq found
+        by increasing the weight of the best actions 
+        then descreasing the weights of all the moves proportionally to their probabilities of being played
+        """
+        alpha = 1
+        polp = np.array(policy)
+        while not st.terminal():
+            moves = st.legal_moves()
+            z = 0
+            for mv in moves:
+                z = z + math.exp(policy[st.code(mv)] + st.heuristic(mv))
+            move = st.seq[len(st.seq)]
+            polp[st.code(move)] += alpha
+
+            for i in range(len(moves)):
+                proba = math.exp(policy[st.code(mv)] + st.heuristic(mv))/z
+                polp[st.code(moves[i])] -= alpha * proba
+
+            st.play(move)
+        
+        return polp
+
+
+
+    def nrpa(self, protein, st:State, level, policy, initial_level):
+        best_state = copy.deepcopy(st)
+        best_state_lip = -1000
+        best_state_kd = 1000
+        
+        pol = np.array(policy)
+
+        while not st.terminal():
+
+            moves = st.legal_moves()
+
+            if len(moves)==0:
+                break
+
+            for mv in moves:
+
+                new_st = copy.deepcopy(st)
+                new_st.play(mv)
+
+                if level <= 1:
+                    new_st = self.playout(st, policy)
+                    return new_st
+                else:
+                    new_st = self.nrpa(protein, st, level - 1, pol, initial_level)
+                
+                new_st_lip = new_st.lipinskiness()
+                new_st_kd = new_st.kd_score(protein)
+                writeline(str(new_st.smile_to_smile(new_st.SMILE))+ " " + str(new_st_kd) +"\n", f"{self.registerName}_dock" )
+        
+
+                if (level <= initial_level -1 and new_st_kd < best_state_kd) or (level == initial_level and new_st_lip > best_state_lip): 
+                    best_state = new_st
+                    best_state_kd = new_st_kd
+                    best_state_lip = new_st_lip
+                    pol = self.adapt(best_state, pol)
+
+            if len(best_state.seq) == len(st.seq):
+                break
+
+            st.play(best_state.seq[len(st.seq)]) 
+            st_smile = st.smile_to_smile(st.SMILE)
+            writeline(str(time.time() - self.start_time)+ " " + st_smile + " " + str(best_state_lip) + " "+ str(best_state_kd) + "\n", f"{self.registerName}_sc" )
+        
+
+        if best_state_kd != 1000 and best_state_lip != -2:
+            smile = st.smile_to_smile(st.SMILE)
+            writeline(str(time.time() - self.start_time)+ " " + smile + " " + str(best_state_lip) + " "+ str(best_state_kd) + "\n", f"{self.registerName}" )
+            
+
+        return st
+
+
+
+def launch_nrpa(protein, init_st: State, level, timeout, register_name):
+
+    expe = NRPA()
+    expe.timeout = timeout
+    expe.registerName = register_name
+
+    policy = [0] * 7 # initial policy for the seven types of legal moves
+
+    st = expe.nrpa(protein, init_st, level, policy, level)
+    return st
