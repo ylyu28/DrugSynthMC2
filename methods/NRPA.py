87c0a863f284cc7837564d713093c0ec9d85743c
ylyu28 <yalilyu28@gmail.com>
2025-12-23 18:33:01 +0000

    update NRPA.py

--- methods/NRPA.py
+++ methods/NRPA.py
@@ -61,74 +61,34 @@ class NRPA:
         
         return polp
 
-    def nrpa(self, protein, rLevel, policy, initLevel):
-        if rLevel == 0:
+    def nrpa(self, protein, r_level, policy, init_level):
+        if r_level == 0:
             return self.playout(State.new(),policy)
-        bestLip = -1000
-        bestKD = 1000
-        bestState = State.new()
+        best_lip = -1000
+        best_kd = 1000
+        best_state = State.new()
         for _ in range(100):
-            newState = self.nrpa(protein,rLevel-1,policy,initLevel)
-            newStateLip = newState.lipinskiness()
-            newStateKD  = newState.kd_score(protein)
-            if (rLevel <= initLevel -1 and newStateKD < bestKD) or (rLevel == initLevel and newStateLip > bestLip):
-                bestLip = newStateLip
-                bestKD = newStateKD
-                bestState = newState
+            new_state = self.nrpa(protein,r_level-1,policy,init_level)
+            new_state_lip = new_state.lipinskiness()
+            new_state_kd  = new_state.kd_score(protein)
+            writeline(str(new_state.smile_to_smile(new_state.SMILE))+ " " + str(new_state_kd) +"\n", f"{self.registerName}_dock" )
+            if (r_level <= init_level -1 and new_state_kd < best_kd) or (r_level == init_level and new_state_lip > best_lip):
+                best_state_lip = new_state_lip
+                best_state_kd = new_state_kd
+                best_state = new_state
+                if best_state_kd != 1000 and best_state_lip != -2:
+                    smile = new_state.smile_to_smile(new_state.SMILE)
+                    writeline(str(time.time() - self.start_time)+ " " + smile + " " + str(best_state_lip) + " "+ str(best_state_kd) + "\n", f"{self.registerName}" )
+
                 # now if bestState gets updated, also update the policy
                 # this updated policy would also be used in the nrpa for the next iteration
-                policy = self.adapt(bestState.seq, policy)
-        return bestState
+                policy = self.adapt(best_state.seq, policy)
+
+        return best_state
 
 
-    # def nrpa(self, protein, st:State, level, policy, initial_level):
-    #     best_state = copy.deepcopy(st)
-    #     best_state_lip = -1000
-    #     best_state_kd = 1000
-        
-    #     polp = np.array(policy)
-
-    #     while not st.terminal():
-    #         moves = st.legal_moves()
-    #         if len(moves)==0:
-    #             break
-
-    #         for mv in moves:
-
-    #             new_st = copy.deepcopy(st)
-    #             new_st.play(mv)
-
-    #             for i in range(100):
-    #                 if level <= 1:
-    #                     new_st = self.playout(st, policy)
-    #                 else:
-    #                     new_st, polp = self.nrpa(protein, st, level - 1, polp, initial_level)
-                    
-    #                 new_st_lip = new_st.lipinskiness()
-    #                 new_st_kd = new_st.kd_score(protein)
-    #                 writeline(str(new_st.smile_to_smile(new_st.SMILE))+ " " + str(new_st_kd) +"\n", f"{self.registerName}_dock" )
-    #                 if (level <= initial_level -1 and new_st_kd < best_state_kd) or (level == initial_level and new_st_lip > best_state_lip): 
-    #                     best_state = new_st
-    #                     best_state_kd = new_st_kd
-    #                     best_state_lip = new_st_lip
-    #                     polp = self.adapt(best_state.seq, polp)
-
-    #         if len(best_state.seq) == len(st.seq):
-    #             break
-
-    #         st.play(best_state.seq[len(st.seq)]) 
-    #         st_smile = st.smile_to_smile(st.SMILE)
-    #         writeline(str(time.time() - self.start_time)+ " " + st_smile + " " + str(best_state_lip) + " "+ str(best_state_kd) + "\n", f"{self.registerName}_sc" )
-        
-
-    #     if best_state_kd != 1000 and best_state_lip != -2:
-    #         smile = st.smile_to_smile(st.SMILE)
-    #         writeline(str(time.time() - self.start_time)+ " " + smile + " " + str(best_state_lip) + " "+ str(best_state_kd) + "\n", f"{self.registerName}" )
-            
-
-    #     return st, polp
     
-def launch_nrpa(protein, init_st: State, level, timeout, register_name):
+def launch_nrpa(protein, level, timeout, register_name):
 
     expe = NRPA()
     expe.timeout = timeout
